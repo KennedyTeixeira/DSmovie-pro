9a990e4a86a969ddc9ad841149c2bdf88db5734b
Kennedy Teixeira <kennedykts@gmail.com>
2022-05-09 19:51:30 -0300

    Projeto criado

---
+++ backend/src/test/java/com/devsuperior/dsmoviepro/DsmovieProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmoviepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
