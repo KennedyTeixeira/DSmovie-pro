874e7d7c02de3b1cfd0333c639128be93b2e168a
Kennedy Teixeira <kennedykts@gmail.com>
2022-05-10 01:38:15 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,41 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
-    
-    return (
-        <Pagination></Pagination>
-    )
+  return (
+    <>
+      <Pagination></Pagination>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div>
+          <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div>         
+          <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div>         
+          <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div>         
+          <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div>
+          <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div>
+          <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div>               
+          <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard></MovieCard>
+          </div>                                             
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
