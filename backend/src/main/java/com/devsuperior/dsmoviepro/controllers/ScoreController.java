dc96ae3f2e807bd7960352ae95a8a191ef9e6ddd
Kennedy Teixeira <kennedykts@gmail.com>
2022-05-11 22:35:33 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmoviepro/controllers/ScoreController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsmoviepro.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmoviepro.dto.MovieDTO;
+import com.devsuperior.dsmoviepro.dto.SocoreDTO;
+import com.devsuperior.dsmoviepro.services.ScoreService;
+
+@RestController
+@RequestMapping(value = "/scores")
+public class ScoreController {
+	
+	@Autowired
+	private ScoreService service;	
+	
+	@PutMapping
+	public MovieDTO saveScore(@RequestBody SocoreDTO dto) {
+		MovieDTO movieDTO = service.saveScore(dto);
+		return movieDTO;
+	}
+
+}
